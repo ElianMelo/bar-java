8228356aa777ab383bd64529762aa4f6298c3abd
ElianMelo <elianmelomorais@gmail.com>
2020-05-06 16:42:24 -0300

    Relatorio gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -12,15 +12,50 @@ public class Programa {
 
 		char sexo;
 		int cervejas, refri, espetos;
+		double consumo, couvert, ingresso, total;
 		
-		System.out.println("Sexo: ");
+		// 1 LEITURA DOS DADOS
+		
+		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
-		System.out.println("Quantidade de cervejas: ");
+		System.out.print("Quantidade de cervejas: ");
 		cervejas = sc.nextInt();
-		System.out.println("Quantidade de refrigerantes: ");
+		System.out.print("Quantidade de refrigerantes: ");
 		refri = sc.nextInt();
-		System.out.println("Quantidade de espetinhos: ");
+		System.out.print("Quantidade de espetinhos: ");
 		espetos = sc.nextInt();
+		
+		// 2 CALCULOS
+		
+		if (sexo == 'F') {
+			ingresso = 8.0;
+		} else {
+			ingresso = 10.0;
+		}
+		
+		consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
+		
+		if (consumo > 30.0) {
+			couvert = 0.0;
+		} else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+		
+		// 3 RELATORIO
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0.0) {
+			System.out.println("Isento de Couvert");
+		} else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc.close();
 	}
