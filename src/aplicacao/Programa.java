3c124236768b64091c639787e607a11e2fae9d54
ElianMelo <elianmelomorais@gmail.com>
2020-05-06 16:31:52 -0300

    Leitura dos dados

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,28 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char sexo;
+		int cervejas, refri, espetos;
+		
+		System.out.println("Sexo: ");
+		sexo = sc.next().charAt(0);
+		System.out.println("Quantidade de cervejas: ");
+		cervejas = sc.nextInt();
+		System.out.println("Quantidade de refrigerantes: ");
+		refri = sc.nextInt();
+		System.out.println("Quantidade de espetinhos: ");
+		espetos = sc.nextInt();
+		
+		sc.close();
+	}
+
+}
